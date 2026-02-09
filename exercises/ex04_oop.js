30871f41de99ce61ccf2ad324c9c3a18280720d4
Salix Nibigira <Salix>
2026-02-09 10:42:17 +0100

    Salix EX 4

--- exercises/ex04_oop.js
+++ exercises/ex04_oop.js
@@ -18,16 +18,20 @@ class Book {
   }
 
   borrow() {
-    // TODO : si déjà emprunté -> false
-    // sinon marquer emprunté -> true
+    if (this.#isBorrowed) {
+      return false;
+    }
+    this.#isBorrowed = true;
+    return true;
   }
 
   returnBook() {
-    // TODO : remettre non emprunté et retourner true
+    this.#isBorrowed = false;
+    return true;
   }
 
   isBorrowed() {
-    // TODO : retourner true/false
+    return this.#isBorrowed;
   }
 }
 
@@ -38,17 +42,22 @@ class User {
   }
 
   borrow(book) {
-    // TODO :
-    // - appeler book.borrow()
-    // - si true : borrowedCount++ et return true
-    // - sinon return false
+    const success = book.borrow();
+    if (success) {
+      this.borrowedCount++;
+      return true;
+    }
+    return false;
   }
 }
 
 class Librarian extends User {
+  constructor(name) {
+    super(name);
+  }
+
   forceReturn(book) {
-    // TODO : forcer le retour du livre (book.returnBook())
-    // retourner true si réussi
+    return book.returnBook();
   }
 }
 
