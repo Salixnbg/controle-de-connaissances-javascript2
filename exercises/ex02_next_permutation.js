d04b0e3c3e1d1105100d8a8ec2278f204e474d62
Salix Nibigira <Salix>
2026-02-09 10:13:09 +0100

    Salix EX 2

--- exercises/ex02_next_permutation.js
+++ exercises/ex02_next_permutation.js
@@ -8,8 +8,37 @@ dans l'ordre lexicographique. Si nums est la dernière permutation, la transform
 */
 
 function nextPermutation(nums) {
-  // TODO : écrire ton code ici
+  // 1. Trouver l'index i où nums[i] < nums[i + 1] en partant de la fin
+  let i = nums.length - 2;
+  while (i >= 0 && nums[i] >= nums[i + 1]) {
+    i--;
+  }
+
+  // 2. Si on a trouvé un tel i, trouver à droite un élément juste plus grand et échanger
+  if (i >= 0) {
+    let j = nums.length - 1;
+    while (nums[j] <= nums[i]) {
+      j--;
+    }
+    let temp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = temp;
+  }
+
+  // 3. Inverser la partie à droite de i (ou tout le tableau si i == -1)
+  let left = i + 1;
+  let right = nums.length - 1;
+
+  while (left < right) {
+    let temp = nums[left];
+    nums[left] = nums[right];
+    nums[right] = temp;
+    left++;
+    right--;
+  }
 }
+
+
 
 // --------------------
 // Tests (au moins 3)
